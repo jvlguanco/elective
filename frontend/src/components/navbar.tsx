036e2c6e83647f459cb5f1b25cee0b4858c81905
jvlguanco <jvlguanco@gmail.com>
2024-09-21 13:43:44 +0800

    Initialized Backend and Front

---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,38 @@
+import { NavLink } from 'react-router-dom';
+import logo from "../assets/Logo.png"
+
+const Navbar = () => {
+  return (
+    <nav className="px-4 py-2 flex justify-between items-center border-t-4 border-yellow-500">
+      <div className="flex gap-4 items-center">
+        <img src={logo} alt="Logo" className="h-20" />
+        <div>
+          <h1 className='font-serif text-lg font-semibold text-yellow-600'>PAMANTASAN NG LUNGSOD NG MAYNILA</h1>
+          <h3 className='text-sm text-gray-600'>University of the City of Manila</h3>
+        </div>
+      </div> 
+      <ul className="flex list-none m-0 p-0">
+        <li className="ml-6">
+          <NavLink to="/" className={({ isActive }) => isActive ? "text-red-500 font-bold" : "text-black"} end>Home</NavLink>
+        </li>
+        <li className="ml-6">
+          <NavLink to="/about" className={({ isActive }) => isActive ? "text-red-500 font-bold" : "text-black"}>About</NavLink>
+        </li>
+        <li className="ml-6">
+          <NavLink to="/academics" className={({ isActive }) => isActive ? "text-red-500 font-bold" : "text-black"}>Academics</NavLink>
+        </li>
+        <li className="ml-6">
+          <NavLink to="/admissions" className={({ isActive }) => isActive ? "text-red-500 font-bold" : "text-black"}>Admissions</NavLink>
+        </li>
+        <li className="ml-6">
+          <NavLink to="/announcements" className={({ isActive }) => isActive ? "text-red-500 font-bold" : "text-black"}>Announcements</NavLink>
+        </li>
+        <li className="ml-6">
+          <NavLink to="/downloads" className={({ isActive }) => isActive ? "text-red-500 font-bold" : "text-black"}>Downloads</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+import Navbar from './components/navbar.tsx';
+import Home from './pages/main_routes/home.tsx'
+import About from './pages/main_routes/about.tsx';
+import Academics from './pages/main_routes/academics.tsx';
+import Admission from './pages/main_routes/admission.tsx';
+import Announcement from './pages/main_routes/announcement.tsx';
+import Download from './pages/main_routes/download.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/academics" element={<Academics />} />
+        <Route path="/admissions" element={<Admission />} />
+        <Route path="/announcements" element={<Announcement />} />
+        <Route path="/downloads" element={<Download />} />
+      </Routes>
+    </BrowserRouter>
+    {/* <App /> */}
   </StrictMode>,
 )
