f310d4a45a4929edfc4ca559b8f3352b8e90dcb3
jvlguanco <jvlguanco@gmail.com>
2024-09-21 14:41:47 +0800

    moved assets

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,11 +1,10 @@
 import { NavLink } from 'react-router-dom';
-import logo from "../assets/Logo.png"
 
 const Navbar = () => {
   return (
     <nav className="px-4 py-2 flex justify-between items-center border-t-4 border-yellow-500">
       <div className="flex gap-4 items-center">
-        <img src={logo} alt="Logo" className="h-20" />
+        <img src='./images/Logo.png' alt="Logo" className="h-20" />
         <div>
           <h1 className='font-serif text-lg font-semibold text-yellow-600'>PAMANTASAN NG LUNGSOD NG MAYNILA</h1>
           <h3 className='text-sm text-gray-600'>University of the City of Manila</h3>
