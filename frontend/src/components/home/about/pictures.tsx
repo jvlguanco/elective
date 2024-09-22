a95320866ba74957f1df5c91d46f2b34bfd9a42d
jvlguanco <jvlguanco@gmail.com>
2024-09-22 12:39:55 +0800

    About Section Collage

--- frontend/src/components/home/about/pictures.tsx
+++ frontend/src/components/home/about/pictures.tsx
@@ -1,7 +1,28 @@
 const Compilation = () => {
     return (
-        <div className="w-1/2 h-[550px] bg-blue-400">
-            Hi
+        <div className="w-2/3 flex items-center justify-center relative">
+
+            <img src="./images/designs/Frame.png" alt="Background" className="absolute w-32 h-32 object-cover top-0 left-40 z-[-1]" />
+
+            <div className="flex flex-col gap-1">
+                <div className="flex gap-1 h-56">
+                    <div className="w-2/3">
+                        <img src="./images/image3.jpg" alt="description" className="object-cover h-full w-full" />
+                    </div>
+                    <div className="w-1/3">
+                        <img src="./images/image3.jpg" alt="description" className="object-cover h-full w-full" />
+                    </div>
+                </div>
+
+                <div className="flex gap-1 h-56">
+                    <div className="w-1/2">
+                        <img src="./images/image3.jpg" alt="description" className="object-cover h-full w-full" />
+                    </div>
+                    <div className="w-1/2">
+                        <img src="./images/image3.jpg" alt="description" className="object-cover h-full w-full" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
