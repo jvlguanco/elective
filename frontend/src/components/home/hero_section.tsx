66cb0f542cc96356f300f7bec5dca7a105a294b5
jvlguanco <jvlguanco@gmail.com>
2024-09-22 22:54:43 +0800

    separated the hero section

---
+++ frontend/src/components/home/hero_section.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Hero = () => {
+    const [image, setImage] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get('http://localhost:5000/image/background')
+        .then((response) => {
+            setImage(response.data);
+        })
+        .catch((error) => {
+            console.error('Error fetching image:', error);
+        });
+    }, []);
+
+    return(
+        <div className="relative w-full h-screen overflow-hidden">
+            {image && (
+                <img src={`data:image/*;base64,${image.data}`}alt="" className='h-full w-full object-cover absolute' />
+            )}
+
+            <div className="absolute top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 z-0"></div>
+
+            <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-between z-10 pt-52 pb-28">
+                <div className="flex flex-col justify-center items-center gap-6">
+                    <img src="./images/Logo.png" alt="" className="h-72 w-72"/>
+                    <div className="flex items-center justify-center flex-col text-white gap-1">
+                        <h1 className="text-2xl font-serif font-medium">PAMANTASAN NG LUNGSOD NG MAYNILA</h1>
+                        <h2 className="text-lg font-light font-inter">University of the City of Manila</h2>
+                        <h3 className="text-base font-thin font-inter">Karunungan, Kaunlaran at Kadakilaan</h3>
+                    </div>
+                </div>
+
+                <div className="mb-12 bg-red-800 text-white px-4 py-2 rounded-full">
+                    APPLY NOW
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
--- frontend/src/pages/main_routes/home.tsx
+++ frontend/src/pages/main_routes/home.tsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Carousel from "../../components/carousel";
 import Compilation from "../../components/home/about/pictures";
 import AboutText from "../../components/home/about/text";
 import AnnouncementSection from "../../components/home/announcement";
+import Hero from "../../components/home/hero_section";
 
 const images = [
     './images/image3.jpg',
@@ -12,19 +11,6 @@ const images = [
 ];
 
 const Home = () => {
-    const [image, setImage] = useState([]);
-
-    useEffect(() => {
-        axios
-        .get('http://localhost:5000/image/background')
-        .then((response) => {
-            setImage(response.data);
-        })
-        .catch((error) => {
-            console.error('Error fetching image:', error);
-        });
-    }, []);
-
     // const [images, setImages] = useState([]);
 
     // useEffect(() => {
@@ -42,28 +28,7 @@ const Home = () => {
 
     return (
         <div>
-            <div className="relative w-full h-screen overflow-hidden">
-                {image && (
-                    <img src={`data:image/*;base64,${image.data}`}alt="" className='h-full w-full object-cover absolute' />
-                )}
-
-                <div className="absolute top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 z-0"></div>
-
-                <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-between z-10 pt-52 pb-28">
-                    <div className="flex flex-col justify-center items-center gap-6">
-                        <img src="./images/Logo.png" alt="" className="h-72 w-72"/>
-                        <div className="flex items-center justify-center flex-col text-white gap-1">
-                            <h1 className="text-2xl font-serif font-medium">PAMANTASAN NG LUNGSOD NG MAYNILA</h1>
-                            <h2 className="text-lg font-light font-inter">University of the City of Manila</h2>
-                            <h3 className="text-base font-thin font-inter">Karunungan, Kaunlaran at Kadakilaan</h3>
-                        </div>
-                    </div>
-
-                    <div className="mb-12 bg-red-800 text-white px-4 py-2 rounded-full">
-                        APPLY NOW
-                    </div>
-                </div>
-            </div>
+            <Hero />
             
             <div className="flex w-full mt-10">
                 <Compilation/>
