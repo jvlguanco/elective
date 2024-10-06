561b3c10113150bdb019c13629fa3f6d82801ee7
jvlguanco <jvlguanco@gmail.com>
2024-10-06 19:29:50 +0800

    added some visual representation in uploads

--- backend/src/pages/home/about.tsx
+++ backend/src/pages/home/about.tsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 const AboutEdit = () => {
     const [content, setContent] = useState<string>('');
     const [message, setMessage] = useState<string>('');
     const [photos, setPhotos] = useState<FileList | null>(null);
+    const [filePaths, setFilePaths] = useState<string[]>([]);
+    const location = "AboutCollage";
+    const fileInputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         axios.get('http://localhost:5000/home/about')
@@ -16,18 +19,32 @@ const AboutEdit = () => {
             });
     }, []);
 
+    useEffect(() => {
+        const fetchPhotos = async () => {
+          try {
+            const response = await fetch(`http://localhost:5000/home/photos/${location}`);
+            const data = await response.json();
+            setFilePaths(data.map((item: { file_path: string }) => item.file_path));
+          } catch (error) {
+            console.error('Error fetching photos:', error);
+          }
+        };
+    
+        fetchPhotos();
+    }, [location]);
+
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setPhotos(e.target.files);
     };
 
     const handleUpload = async (e: React.FormEvent) => {
         e.preventDefault();
-        if (photos && photos.length > 0) {
+        if (photos && photos.length != 4) {
           const formData = new FormData();
           for (let i = 0; i < photos.length; i++) {
             formData.append('photos', photos[i]);
           }
-          formData.append('location', 'AboutCollage');
+          formData.append('location', location);
     
           try {
             const res = await axios.post(
@@ -38,11 +55,15 @@ const AboutEdit = () => {
               }
             );
             setMessage(res.data);
+
+            if (fileInputRef.current) {
+                fileInputRef.current.value = '';
+            }
           } catch (error) {
             setMessage('Failed to upload photos.');
           }
         } else {
-          setMessage('Please select at least 1 photo.');
+          setMessage('Please select a total of 4 photos.');
         }
     };
 
@@ -60,50 +81,70 @@ const AboutEdit = () => {
 
     const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setContent(e.target.value);
-        // Reset the message when the user starts typing
+
         if (message) {
             setMessage('');
         }
     };
 
+    useEffect(() => {
+        if (message) {
+            alert(message);
+            setMessage('');
+        }
+    }, [message]);
+
     return (
-        <div className="w-full flex flex-col gap-6">
-            <h2 className="text-2xl font-bold">Edit About Message at Home Page</h2>
+        <div className="w-full flex flex-col">
+            <h2 className="text-2xl font-bold mb-4">Edit About Message at Home Page</h2>
             
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className='mb-8'>
                 <textarea
-                    className="w-full h-96 p-2 border rounded mb-4 overflow-y-auto"
+                    className="w-full h-56 p-2 border rounded mb-2 overflow-y-auto"
                     value={content}
                     onChange={handleContentChange}
                 ></textarea>
                 <button
                     type="submit"
-                    className="px-4 py-2 bg-blue-500 text-white rounded"
+                    className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" 
                 >
                     Update Paragraph
                 </button>
             </form>
 
-            {message && <p className="text-green-800 mb-4">{message}</p>}
+            <h1 className="text-2xl font-bold mb-2">Change Photo Collage in the About Section</h1>
 
-            <h1 className="text-2xl font-bold mb-4">Upload Photos</h1>
+            <div className='h-80 w-full flex gap-2'>
+                {filePaths ? (
+                    filePaths.map((photo, index) => (
+                        <img
+                          key={index}
+                          src={`http://localhost:5000/${photo}`}
+                          alt={`Uploaded Photo ${index + 1}`}
+                          className="w-1/4 h-full object-cover"
+                        />
+                    ))
+                ) : (
+                    <p>No file found</p>
+                )}
+            </div>
 
-            <form onSubmit={handleUpload} className="flex flex-col items-center">
+            <form onSubmit={handleUpload} className="flex flex-col mt-4">
                 <input
                 type="file"
                 accept="image/*"
                 multiple
                 onChange={handleFileChange}
-                className="mb-4"
+                ref={fileInputRef}
+                className="mb-2"
                 />
                 <button
                 type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+                className="bg-blue-500 text-white px-4 py-2 rounded w-fit hover:bg-blue-700"
                 >
                 Upload
                 </button>
             </form>
-            {message && <p className="mt-4">{message}</p>}
         </div>
     );
 }
