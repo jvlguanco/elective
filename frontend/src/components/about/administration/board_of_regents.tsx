7ea6b9475a3bb4f8eb9983e998ef0729b26b0180
jvlguanco <jvlguanco@gmail.com>
2024-11-06 00:58:13 +0800

    Board of Regents Front

---
+++ frontend/src/components/about/administration/board_of_regents.tsx
@@ -0,0 +1,84 @@
+const BoardOfRegents = () => {
+    return(
+        <div className="w-full pt-8 pr-12">
+            <h1 className="pb-2 border-b-2 border-gray-400 font-inter font-semibold text-[32px] text-navy-blue">
+                BOARD OF REGENTS
+            </h1>
+
+            <p className="mt-2 mb-4">
+            The Board of Regents, the highest policy-making body of the university, sets and steers the direction of the university to fulfill its vision and mission.<br/><br/>
+
+            The Board presents a stellar lineup comprised of esteemed professionals in their respective fields. They bring with them expertise that would help PLM realize its goals.<br/><br/>
+
+            The Chairperson of the Board and the University President are elected from among the regents themselves. The Chairperson presides over all the meetings of the Board, while the University President serves as its ex-officio Vice-Chair.
+            </p>
+
+            <div className="grid grid-cols-3 gap-6 mt-12">
+                <div className="flex flex-col items-center">
+                    <img src="/images/temp.png" alt="Regent 1" className="w-4/6 h-4/5 object-cover" />
+                    <p className="font-semibold mt-2">Name 1</p>
+                    <p className="text-gray-500">Title 1</p>
+                    <p className="text-gray-400">Description 1</p>
+                </div>
+                <div className="flex flex-col items-center">
+                    <img src="/images/temp.png" alt="Regent 2" className="w-4/6 h-4/5 object-cover" />
+                    <p className="font-semibold mt-2">Name 2</p>
+                    <p className="text-gray-500">Title 2</p>
+                    <p className="text-gray-400">Description 2</p>
+                </div>
+                <div className="flex flex-col items-center">
+                    <img src="/images/temp.png" alt="Regent 3" className="w-4/6 h-4/5 object-cover" />
+                    <p className="font-semibold mt-2">Name 3</p>
+                    <p className="text-gray-500">Title 3</p>
+                    <p className="text-gray-400">Description 3</p>
+                </div>
+
+                <div className="flex flex-col items-center">
+                    <img src="/images/temp.png" alt="Regent 4" className="w-4/6 h-4/5 object-cover" />
+                    <p className="font-semibold mt-2">Name 4</p>
+                    <p className="text-gray-500">Title 4</p>
+                    <p className="text-gray-400">Description 4</p>
+                </div>
+                <div className="flex flex-col items-center">
+                    <img src="/images/temp.png" alt="Regent 5" className="w-4/6 h-4/5 object-cover" />
+                    <p className="font-semibold mt-2">Name 5</p>
+                    <p className="text-gray-500">Title 5</p>
+                    <p className="text-gray-400">Description 5</p>
+                </div>
+                <div className="flex flex-col items-center">
+                    <img src="/images/temp.png" alt="Regent 6" className="w-4/6 h-4/5 object-cover" />
+                    <p className="font-semibold mt-2">Name 6</p>
+                    <p className="text-gray-500">Title 6</p>
+                    <p className="text-gray-400">Description 6</p>
+                </div>
+
+                <div className="flex flex-col items-center">
+                    <img src="/images/temp.png" alt="Regent 7" className="w-4/6 h-4/5 object-cover" />
+                    <p className="font-semibold mt-2">Name 7</p>
+                    <p className="text-gray-500">Title 7</p>
+                    <p className="text-gray-400">Description 7</p>
+                </div>
+            </div>
+
+            <h2 className="font-inter font-semibold text-[24px] text-navy-blue mt-2">
+                Composition
+            </h2>
+
+            <p className="mt-2 mb-4">
+            The Board of Regents is a collegial body composed of seven (7) highly distinguished members, six (6) of whom are appointed by the Mayor of the city of Manila, with the consent of the then-Municipal Board, which is now the City Council of Manila. Of the six members appointed by the Mayor, the PLM Charter prescribes that one comes from the faculty, another is a distinguished alumnus, and one of the others from the field of education. The university regents are chosen on the basis of their professional competence. They are men of high and eminent integrity and distinguished professionals among the citizenry. The Division of City Schools (DCS) superintendent of Manila sits as an ex-officio member of the Board of Regents and becomes the 7th member of the Board.
+            </p>
+
+            <h2 className="font-inter font-semibold text-[24px] text-navy-blue mt-8">
+                Term of Office
+            </h2>
+
+            <p className="mt-2 mb-4">
+            Confirmed regential appointees automatically serve for a fixed term of six (6) years or until their successors shall have been appointed and qualified. They may therefore serve for more than 6 years on a hold-over capacity, provided no replacement shall have been qualified as of yet.<br/><br/>
+            In case an incumbent regent cannot finish his term of six years by reason of resignation, death or other incapacities, the vacancy shall be filled by an appointment by the Mayor of the City of Manila, with the consent of the City Council, and the appointee shall hold office for the unexpired portion of the term, instead of the fixed term of six (6) years.
+            </p>
+
+        </div>
+    )
+}
+
+export default BoardOfRegents;  
